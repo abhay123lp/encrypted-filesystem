87f14ae2d0b2549fa957aaa43df16db03f7c62e4
mikhail.beloshapkin@gmail.com <mikhail.beloshapkin@gmail.com@7e372b31-7676-3320-43bc-f00a640c0703>
2013-08-29 04:33:24 +0000

    Cipher utils changing and adding tests.

---
+++ trunk/EncryptedFilesystem/trunk/encrypted-filesystem/src/test/java/com/bm/nio/file/CipherUtilsTest.java
@@ -0,0 +1,96 @@
+package com.bm.nio.file;
+
+import java.security.GeneralSecurityException;
+
+import javax.crypto.Cipher;
+
+import junit.framework.Assert;
+
+import org.junit.Test;
+
+import com.bm.nio.utils.CipherUtils;
+import com.bm.nio.utils.CipherUtilsImplException;
+import com.bm.nio.utils.impl.CipherUtilsImplStandard;
+
+public class CipherUtilsTest {
+
+	static class ImplIncorrect extends CipherUtilsImplStandard {
+
+		@Override
+		public byte[] decryptBlockImpl(Cipher decipher, byte[] bufEnc,
+				int start, int len) throws GeneralSecurityException {
+			return super.decryptBlockImpl(decipher, bufEnc, start, len);
+		}
+
+		@Override
+		public byte[] encryptBlockImpl(Cipher encipher, byte[] bufPlain,
+				int start, int len) throws GeneralSecurityException {
+			return super.encryptBlockImpl(encipher, bufPlain, start, len);
+		}
+
+	}
+	
+	static class ImplCorrect extends ImplIncorrect {
+
+		@Override
+		public byte[] decryptBlockImpl(Cipher decipher, byte[] bufEnc,
+				int start, int len) throws GeneralSecurityException {
+			return super.decryptBlockImpl(decipher, bufEnc, start, len);
+		}
+
+		@Override
+		public byte[] encryptBlockImpl(Cipher encipher, byte[] bufPlain,
+				int start, int len) throws GeneralSecurityException {
+			return super.encryptBlockImpl(encipher, bufPlain, start, len);
+		}
+
+		@Override
+		public int getEncAmtImpl(Cipher encipher, int decAmt) {
+			return super.getEncAmtImpl(encipher, decAmt);
+		}
+
+	}
+	@Test
+	public void testSetImpl() throws Exception {
+		boolean exception;
+		CipherUtils.resetImpl();
+		
+		// not all required methods are overriden
+		exception = false;
+		try {
+			CipherUtils.setImpl(new ImplIncorrect());
+		} catch (CipherUtilsImplException e) {
+			exception = true;
+		}
+		Assert.assertTrue(exception);
+		
+		//correctly override all required methods
+		exception = false;
+		try {
+			CipherUtils.setImpl(new ImplCorrect());
+		} catch (CipherUtilsImplException e) {
+			exception = true;
+		}
+		Assert.assertFalse(exception);
+		
+		//doesn't allow to set more than once
+		exception = false;
+		try {
+			CipherUtils.setImpl(new ImplCorrect());
+		} catch (CipherUtilsImplException e) {
+			exception = true;
+		}
+		Assert.assertTrue(exception);
+		
+		//... unless reset
+		exception = false;
+		try {
+			CipherUtils.resetImpl();
+			CipherUtils.setImpl(new ImplCorrect());
+		} catch (CipherUtilsImplException e) {
+			exception = true;
+		}
+		Assert.assertFalse(exception);
+	}
+	
+}
