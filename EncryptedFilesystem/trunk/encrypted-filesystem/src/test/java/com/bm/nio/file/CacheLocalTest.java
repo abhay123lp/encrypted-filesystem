8c00eb483f67f861c21efa57ca980ffaa8132bf5
mikhail.beloshapkin <mikhail.beloshapkin@gmail.com>
2013-08-29 08:21:09 +0000

    Started local cache implementation for ciphers.

---
+++ EncryptedFilesystem/trunk/encrypted-filesystem/src/test/java/com/bm/nio/file/CacheLocalTest.java
@@ -0,0 +1,15 @@
+package com.bm.nio.file;
+
+import junit.framework.Assert;
+
+import org.junit.Test;
+
+public class CacheLocalTest {
+
+	@Test
+	public void testGetCachedObject(){
+		//TODO:
+		Assert.assertTrue(true);
+	}
+	
+}
